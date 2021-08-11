42b894a650a7f8a79e006ae365c8da0975ca614a
antonlavrinov <alv.anton.lavrinov@gmail.com>
2021-08-11 10:47:11 +0300

    added pwa

--- pages/_document.js
+++ pages/_document.js
@@ -32,10 +32,19 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang="ru">
+      <Html lang="ru">  
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com"/>
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700&display=swap" rel="stylesheet"/>
+          {/* <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" /> */}
+          <link rel="manifest" href="/manifest.json" />
+          {/* <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+          <link rel="shortcut icon" href="/favicon/favicon.ico" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+          <meta name="theme-color" content="#ffffff" /> */}
         </Head>
         <body>
           <Main />
