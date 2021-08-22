b58961461500cb8de22a646972ab078ef0de15cc
antonlavrinov <alv.anton.lavrinov@gmail.com>
2021-08-22 17:37:49 +0300

    hihjhj

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
-module.exports = {
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ["@svgr/webpack"]
-      });
-  
-      return config;
-    }
-  };
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public'
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+
+    return config;
+  }
+});
