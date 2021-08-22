39e963619456a9819efaecf7be6c8a4e6798e644
antonlavrinov <alv.anton.lavrinov@gmail.com>
2021-08-23 02:39:53 +0300

    dsdssd

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = withPWA({
     dest: 'public',
     fallbacks: {
       // image: '/static/images/fallback.png',
-      document: '/other-offline.js',  // if you want to fallback to a custom page other than /_offline
+      document: '../pages/other-offline.js',  // if you want to fallback to a custom page other than /_offline
       // font: '/static/font/fallback.woff2',
       // audio: ...,
       // video: ...,
