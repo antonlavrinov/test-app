423fb07c3372ddfef53603426e1ed828ae85c512
antonlavrinov <alv.anton.lavrinov@gmail.com>
2021-11-10 05:43:14 +0300

    sddssd

--- next.config.js
+++ next.config.js
@@ -3,13 +3,6 @@ const withPWA = require('next-pwa')
 module.exports = withPWA({
   pwa: {
     dest: 'public',
-    fallbacks: {
-      // image: '/static/images/fallback.png',
-      document: '../pages/other-offline.js',  // if you want to fallback to a custom page other than /_offline
-      // font: '/static/font/fallback.woff2',
-      // audio: ...,
-      // video: ...,
-    }
   },
   webpack(config) {
     config.module.rules.push({
